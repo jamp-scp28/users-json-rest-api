9efa0199f6a476bf0d9c069907257cf98cb5a795
jamp-scp28 <jorge.molano28@gmail.com>
2022-09-18 14:33:41 -0500

    updates

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import routes from "./rest/routes/index.route";
 const app = express();
 
 app.use(function (req: express.Request, res: express.Response, next: any) {
-    res.setHeader('Access-Control-Allow-Origin', "http://localhost:4200");
+    res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
